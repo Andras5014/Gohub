f599c784d2c4d26b8b9720015a793a7576e70872
Andras <ruijiang5014@gmail.com>
2024-07-25 01:57:45 +0800

    tencent sms

---
+++ internal/service/sms/tencent/sevice_test.go
@@ -0,0 +1,55 @@
+package tencent
+
+import (
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+	"os"
+	"testing"
+)
+
+func TestService_Send(t *testing.T) {
+	secretId, ok := os.LookupEnv("TENCENT_SMS_SECRET_ID")
+	if !ok {
+		t.Skip("TENCENT_SMS_SECRET_ID not set")
+	}
+	secretKey, ok := os.LookupEnv("TENCENT_SMS_SECRET_KEY")
+	c, err := sms.NewClient(common.NewCredential(secretId, secretKey),
+		"ap-chengdu",
+		profile.NewClientProfile())
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := NewService(c, "1400603310", "腾讯云短信测试")
+
+	testCases := []struct {
+		name    string
+		tplId   string
+		params  []string
+		numbers []string
+		wantErr error
+	}{
+		{
+			name:    "test",
+			tplId:   "1400603310",
+			params:  []string{"123456"},
+			numbers: []string{"+86123456789"},
+			wantErr: nil,
+		},
+		{
+			name:    "test",
+			tplId:   "1400603310",
+			params:  []string{"123456"},
+			numbers: []string{"+86123456789"},
+			wantErr: nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := s.Send(nil, tc.tplId, tc.params, tc.numbers...)
+			if err != tc.wantErr {
+				t.Fatalf("want %v, got %v", tc.wantErr, err)
+			}
+		})
+	}
+}
