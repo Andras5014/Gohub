651260bae8e22a9224024a9d0838478f7ca4ac37
Andras <ruijiang5014@gmail.com>
2024-08-03 20:01:29 +0800

    failover

---
+++ internal/service/sms/failover/service.go
@@ -0,0 +1,48 @@
+package failover
+
+import (
+	"context"
+	"errors"
+	"github.com/Andras5014/webook/internal/service/sms"
+	"sync/atomic"
+)
+
+type FailoverSmsService struct {
+	svcs []sms.Service
+	idx  uint64
+}
+
+func NewFailoverSmsService(svcs []sms.Service) sms.Service {
+	return &FailoverSmsService{
+		svcs: svcs,
+	}
+}
+func (f *FailoverSmsService) Send(ctx context.Context, tpl string, args []sms.NamedArg, numbers ...string) error {
+
+	for _, svc := range f.svcs {
+		err := svc.Send(ctx, tpl, args, numbers...)
+		// 发送成功
+		if err == nil {
+			return nil
+		}
+	}
+	return errors.New("所有服务商都发送失败")
+}
+
+func (f *FailoverSmsService) SendV1(ctx context.Context, tpl string, args []sms.NamedArg, numbers ...string) error {
+	// 去下一个节点作为起始节点
+	idx := atomic.AddUint64(&f.idx, 1)
+	length := uint64(len(f.svcs))
+	for i := idx; i < idx+length; i++ {
+		svc := f.svcs[i%length]
+		err := svc.Send(ctx, tpl, args, numbers...)
+		// 发送成功
+		switch err {
+		case nil:
+			return nil
+		case context.DeadlineExceeded, context.Canceled:
+			return err
+		}
+	}
+	return errors.New("所有服务商都发送失败")
+}
---
+++ internal/service/sms/failover/timeout_failover.go
@@ -0,0 +1,52 @@
+package failover
+
+import (
+	"context"
+	"github.com/Andras5014/webook/internal/service/sms"
+	"sync/atomic"
+)
+
+type TimeoutFailoverSmsService struct {
+	svcs []sms.Service
+	// 连续超时个数
+	cnt int32
+	idx int32
+
+	//阈值
+	threshold int32
+}
+
+func NewTimeoutFailoverSmsService(svcs []sms.Service) *TimeoutFailoverSmsService {
+	return &TimeoutFailoverSmsService{
+		svcs:      svcs,
+		threshold: 3,
+		idx:       0,
+		cnt:       0,
+	}
+}
+
+func (f *TimeoutFailoverSmsService) Send(ctx context.Context, tpl string, args []sms.NamedArg, numbers ...string) error {
+	idx := atomic.LoadInt32(&f.idx)
+	cnt := atomic.LoadInt32(&f.cnt)
+
+	if cnt > f.threshold {
+		newIdx := (idx + 1) % int32(len(f.svcs))
+		if atomic.CompareAndSwapInt32(&f.idx, idx, newIdx) {
+			// 切换到newIdx
+			atomic.StoreInt32(&f.cnt, 0)
+		}
+		idx = atomic.LoadInt32(&f.idx)
+	}
+
+	svc := f.svcs[idx]
+	err := svc.Send(ctx, tpl, args, numbers...)
+	switch err {
+	case context.DeadlineExceeded:
+		atomic.AddInt32(&f.cnt, 1)
+	case nil:
+		atomic.StoreInt32(&f.cnt, 0)
+	default:
+		return err
+	}
+	return nil
+}
